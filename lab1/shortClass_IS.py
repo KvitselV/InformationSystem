dc78fbf89c31a978ebd3aca838e1a9701b44bc05
KvitselV <113378172+KvitselV@users.noreply.github.com>
2024-11-26 22:04:12 +0300

    8, 9. Создать класс, содержащий краткую версию данных исходного класса, Собрать два класса в одну иерархию наследования

--- lab1/shortClass_IS.py
+++ lab1/shortClass_IS.py
@@ -1,6 +1,7 @@
 class ShortSoiskatel(Soiskatel):
     def __init__(self, fam, imya, otchestvo):
-        super().__init__(fam, imya, otchestvo, "", "", date(1900, 1, 1), "", "")
-    
+        super().__init__(fam, imya, otchestvo, kvalifikaciya="N/A", professiya="N/A",
+                         data_rozhdeniya=date(1900, 1, 1), telefon="0000000000", adres="N/A")
+
     def __str__(self):
-        return f"{self.fam} {self.imya[0]}. {self.otchestvo[0]}."
+        return f"ShortSoiskatel: {self.get_fam()} {self.get_imya()[0]}. {self.get_otchestvo()[0] if self.get_otchestvo() else ''}."
